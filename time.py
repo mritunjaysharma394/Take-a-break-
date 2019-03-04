7039309d1af44565fdccaec257a991970ea4d5e0
mritunjaysharma394 <45476902+mritunjaysharma394@users.noreply.github.com>
2019-03-04 22:23:29 +0530

    Add files via upload

---
+++ time.py
@@ -0,0 +1,15 @@
+import time
+import webbrowser
+   
+
+
+c=0
+
+while c<4:
+      time.sleep(2*60*60)
+
+      webbrowser.open("https://www.youtube.com/watch?v=5A_vK1cYSoY")
+      c=c+1;
+      
+  
+  
